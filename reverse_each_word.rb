09c499ad004f5887bffa4b419aaa3ee19ffdaa86
Kyle Luke <th.oldschool@gmail.com>
2019-08-20 04:11:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,8 +9,6 @@ end
 
 def reverse_each_word(string)
   new_string = string.split
-  new_string.collect{|word|
-    word.reverse
-  }
+  new_string.collect{|word| word.reverse}
   p new_string.join(" ")
 end
